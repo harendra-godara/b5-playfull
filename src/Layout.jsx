9663571fd63d0d6cc7cfd3ae3fb49ac8e281966b
Harendra Godara <harendragodara880@gmail.com>
2026-06-02 16:27:48 +0530

    add page not found

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import {Outlet} from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
-        <Navbar />
-        <Outlet />
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route , RouterProvider } from 'react-router-dom'
-import Layout from './Layout'
-import Home from './components/Pages/Home'
-import About from './components/Pages/About'
-import Contact from './components/Pages/Contact'
-import Projects from './components/Pages/Projects'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+import Projects from "./components/Pages/Projects";
+import NotFound from "./components/Pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />}/>
-      <Route path='projects' element={<Projects/>}/>
-
-    </Route>
-  )
-)
-createRoot(document.getElementById('root')).render(
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>,
+  ),
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>
-)
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
